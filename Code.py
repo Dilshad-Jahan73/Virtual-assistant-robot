a9d81f5dfef7c7d3836efae93fb2968923c3b0a8
Dilshad-Jahan73 <dilshadjahan73@gmail.com>
2022-04-09 12:47:08 +0600

    joke-wikipedia-can-not-understand-added

--- Code.py
+++ Code.py
@@ -2,7 +2,8 @@ import speech_recognition as sr
 import pyttsx3
 import datetime
 import pywhatkit
-
+import wikipedia
+import pyjokes
 listener = sr.Recognizer()
 cortana = pyttsx3.init()
 # voices = cortana.getProperty('voices')
@@ -19,8 +20,10 @@ def take_command():
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
+            print(command)
             if 'cortana' in command:
-                command = command.replace('cortana', '')
+                command = command.replace('cortana', ' ')
+                #print(command)
 
     except:
         pass
@@ -33,9 +36,26 @@ def run_cortana():
         time = datetime.datetime.now().strftime('%I:%M %p')
         print(time)
         talk('Current time is ' + time)
+# play_song_on_youtube
     elif 'play' in command:
         song = command.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+#tell_me_about_info_from_wikipedia
+    elif 'tell me about' in command:
+        look_for = command.replace('tell me about', ' ')
+        info = wikipedia.summary(look_for, 1)
+        print(info)
+        talk(info)
+#tell_me_joke
+    elif 'joke' in command:
+        jokes = pyjokes.get_joke()
+        print(jokes)
+        talk(jokes)
+#when_did_not_understand
+    else:
+        talk('Sorry, I can not hear you but i can search it for you')
+        pywhatkit.search(command)
 
-run_cortana()
+while True:
+    run_cortana()
